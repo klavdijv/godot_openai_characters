6068ed0beedf011f6defa133ca49ca2705e4b293
klavdij <klavdij@localhost>
2024-08-01 23:29:57 +0200

    update

--- scenes/ai_function_group.gd
+++ scenes/ai_function_group.gd
@@ -4,6 +4,6 @@ extends Node
 func to_json() -> Array[Dictionary]:
 	var json_data: Array[Dictionary] = []
 	for child in get_children():
-		if child is AIFunctions:
+		if child is AIFunctions or child is AIFunctionGroup:
 			json_data.append_array(child.to_json())
 	return json_data
